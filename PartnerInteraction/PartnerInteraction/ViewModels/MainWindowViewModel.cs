6ec276a6f1d725864d58d470289c71ef222d2509
anschek <anschek@yandex.ru>
2025-02-17 17:06:49 +0300

    perf: algorithm for working with messages has been changed

--- PartnerInteraction/PartnerInteraction/ViewModels/MainWindowViewModel.cs
+++ PartnerInteraction/PartnerInteraction/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Media;
 using PartnerInteraction.Views;
 using ReactiveUI;
+using System.Threading.Tasks;
 
 namespace PartnerInteraction.ViewModels
 {
@@ -11,7 +13,24 @@ namespace PartnerInteraction.ViewModels
 
         public MainWindowViewModel()
         {
-            CurrentView = new PartnersListView();
+            CurrentView = new PartnersListView(this);
+        }
+
+        private string _mesage;
+        private SolidColorBrush _messageColor = new SolidColorBrush(Colors.Black);
+        public string Message { get => _mesage; set => this.RaiseAndSetIfChanged(ref _mesage, value); }
+        public SolidColorBrush MessageColor { get => _messageColor; set => this.RaiseAndSetIfChanged(ref _messageColor, value); }
+        public void SetError(string text)
+        {
+            MessageColor = new SolidColorBrush(Colors.Red);
+            Message = text;
+        }
+        public async Task SetSuccess(string text)
+        {
+            MessageColor = new SolidColorBrush(Colors.Black);
+            Message = text;
+            await Task.Delay(5000);
+            Message = "";
         }
     }
 }
