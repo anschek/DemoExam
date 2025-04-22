23a3dacbb543bd318318dfa867318046339b09d8
anschek <anschek@yandex.ru>
2025-04-22 14:31:05 +0300

    feat: add lib for data processing

---
+++ RatingCalculationLib/RatingCalculation.cs
@@ -0,0 +1,56 @@
+﻿using DynamicData;
+using Microsoft.EntityFrameworkCore;
+using StaffQualificationAssessment.Models;
+using System;
+
+namespace RatingCalculationLib
+{
+    public class RatingCalculation
+    {
+        private readonly PmContext _db;
+        public RatingCalculation(PmContext db)
+        {
+            _db = db;
+        }
+
+        public double GetAbsoluteRating(int userId) 
+            => _db.EmployeeMetrics.Include(em => em.Metric)
+            .Where(em => em.StaffId == userId)
+            .Sum(em => em.Metric.Weight) ?? 0; 
+        private List<KeyValuePair<int, double>> GetRatingDictByIds(IEnumerable<int> userIds)
+            => userIds.ToDictionary(id => id, GetAbsoluteRating)
+                    .OrderByDescending(item => item.Value).ToList();
+        public int GetUserRelativeRank(IEnumerable<int> userIds, int userId)
+        {
+            try
+            {
+                var relativeRating = GetRatingDictByIds(userIds);
+                return relativeRating.FindIndex(item => item.Key == userId) + 1;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        public IEnumerable<RatingResult> GetRatingList(IEnumerable<int> userIds, int userId)
+        {
+            try
+            {
+                var relativeRating = GetRatingDictByIds(userIds);
+                var absoluteRating = GetRatingDictByIds(_db.Staff.Select(s => s.Id));
+
+                return userIds.Select(id => new RatingResult
+                {
+                    UserId = id,
+                    RelativeRank = relativeRating.FindIndex(item => item.Key == id) + 1,
+                    AbsoluteRank = absoluteRating.FindIndex(item => item.Key == id) + 1,
+                    AbsoluteRating = absoluteRating.Find(item => item.Key == id).Value
+                });
+            }
+            catch
+            {
+                return [];
+            }
+        }
+    }
+}
---
+++ RatingCalculationLib/RatingResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RatingCalculationLib
+{
+    public class RatingResult
+    {
+        public int UserId { get; set; }
+        public double AbsoluteRating { get; set; }
+        public int AbsoluteRank { get; set; }
+        public int RelativeRank { get; set; }
+    }
+}
